413ba483cdca374e383958463bd2aa5e1e78544d
anjana502 <anjana.nallanagula@gmail.com>
2026-01-15 16:53:29 +0000

    Code challenge submission for question: 13 Roman to Integer

---
+++ 13 Roman to Integer/13roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        n = 0
+        i = 0
+
+        while (i < len(s)):
+            if ((i + 1 < len(s)) and (((s[i] == "I") and (s[i+1] == "V" or s[i+1] == "X")) or ((s[i] == "X") and (s[i+1] == "L" or s[i+1] == "C")) or ((s[i] == "C") and (s[i+1] == "D" or s[i+1] == "M")))):
+                n += d[s[i + 1]] - d[s[i]]
+                i += 2
+            else:
+                n += d[s[i]]
+                i += 1
+        
+        return n
