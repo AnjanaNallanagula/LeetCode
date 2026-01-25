53222979b611ffb9b764f1d76ae5578f600cf44f
anjana502 <anjana.nallanagula@gmail.com>
2026-01-25 21:08:40 +0000

    Code challenge submission for question: 1934 Confirmation Rate

---
+++ 1934 Confirmation Rate/1934confirmation-rate.sql
@@ -0,0 +1,4 @@
+SELECT S.user_id, ROUND(SUM(IF(C.action = "confirmed", 1, 0)) / COUNT(*), 2) AS confirmation_rate FROM Signups S
+LEFT OUTER JOIN Confirmations C ON
+S.user_id = C.user_id
+GROUP BY S.user_id;
