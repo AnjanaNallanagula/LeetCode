f71ed8ad5b403d8f7f708d04b461d9a11920c3d7
anjana502 <anjana.nallanagula@gmail.com>
2026-02-09 04:05:55 +0000

    Code challenge submission for question: 1094 Car Pooling

---
+++ 1094 Car Pooling/1094car-pooling.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        q = []
+        heapq.heapify(q)
+        trips.sort(key = lambda i: i[1])
+
+        for i in range(len(trips)):
+            heapq.heappush(q, (trips[i][1], 1, trips[i][0]))
+            heapq.heappush(q, (trips[i][2], 0, trips[i][0]))
+        
+        while (len(q) != 0):
+            time, flag, count = heapq.heappop(q)
+
+            if (flag == 1):
+                if (count > capacity):
+                    return False
+                
+                capacity -= count
+            else:
+                capacity += count
+        
+        return True
