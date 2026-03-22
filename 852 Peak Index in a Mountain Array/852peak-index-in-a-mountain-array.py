3805a64fb7781ee2288d2ef8168cb2c72f590a9a
anjana502 <anjana.nallanagula@gmail.com>
2026-03-22 19:53:41 +0000

    Code challenge submission for question: 852 Peak Index in a Mountain Array

---
+++ 852 Peak Index in a Mountain Array/852peak-index-in-a-mountain-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        
+        if (arr[0] > arr[1]):
+            return 0
+        if (arr[n - 1] > arr[n - 2]):
+            return (n - 1)
+        
+        low = 1
+        high = n - 2
+        
+        while (low <= high):
+            mid = low + (high - low) // 2
+            
+            if (arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]):
+                return mid
+            if (arr[mid] > arr[mid + 1]):
+                high = mid - 1
+            else:
+                low = mid + 1
