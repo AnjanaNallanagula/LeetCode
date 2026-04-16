2991236d16047e915eb92206e137b6eae2c9b00b
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-04-16 11:32:43 -0600

    Time: 76 ms (42.73%) | Memory: 19.8 MB (16.20%) - LeetSync

---
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -0,0 +1,27 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+
+        for i in strs:
+            d = "{:4}".format(str(len(i)))
+            s += (d + i)
+        
+        return s
+
+    def decode(self, s: str) -> List[str]:
+        ls = []
+        i = 0
+        n = 0
+
+        while (i < len(s)):
+            n = int(s[i: i + 4])
+            i += 4
+            s1 = s[i: i + n]
+            ls.append(s1)
+            i += n
+        
+        return ls
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
