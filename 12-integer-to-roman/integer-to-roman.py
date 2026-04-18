b0a9308fc5cb869887523a9e27a40ea996b79a8f
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-04-18 14:36:09 -0600

    Time: 15 ms (6.30%) | Memory: 19.1 MB (92.78%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -3,11 +3,10 @@ class Solution:
         d = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1}
         s = ""
 
-        while (num > 0):
-            for i in d:
-                if (num >= d[i]):
-                    q = num // d[i]
-                    s = s + (q * i)
-                    num = num - (q * d[i])
+        for i in d:
+            if (num >= d[i]):
+                q = num // d[i]
+                s = s + (q * i)
+                num = num - (q * d[i])
         
         return s
