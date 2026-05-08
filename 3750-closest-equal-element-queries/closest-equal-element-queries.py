e95f4adf87ce23f41f83ed059bc9472e75dc4c4d
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-05-08 08:05:51 -0600

    Time: 863 ms (5.00%) | Memory: 53.9 MB (79.27%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+class Solution:
+    def binary_search_lower_bound(self, ls, target):
+        index = -1
+        low = 0
+        high = len(ls) - 1
+
+        while (low <= high):
+            mid = low + (high - low) // 2
+
+            if (ls[mid] < target):
+                index = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        if (index != -1):
+            return ls[index]
+        return ls[-1]
+    
+    def binary_search_upper_bound(self, ls, target):
+        index = -1
+        low = 0
+        high = len(ls) - 1
+
+        while (low <= high):
+            mid = low + (high - low) // 2
+
+            if (ls[mid] > target):
+                index = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        if (index != -1):
+            return ls[index]
+        return ls[0]
+    
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        d = defaultdict(list)
+        n = len(nums)
+
+        for i in range(n):
+            d[nums[i]].append(i)
+        
+        ls = []
+
+        for i in queries:
+            target = nums[i]
+
+            if (len(d[target]) == 1):
+                ls.append(-1)
+            else:
+                l = self.binary_search_lower_bound(d[target], i)
+                r = self.binary_search_upper_bound(d[target], i)
+
+                print((l, r))
+
+                min1 = float("inf")
+
+                if (l != -1):
+                    min1 = min(min1, (i - l) % n)
+                if (r != -1):
+                    min1 = min(min1, (r - i) % n)
+                
+                ls.append(min1)
+        
+        return ls
