678352733fdc32c05adde48547aef0e26b550b96
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-07-21 21:10:32 -0600

    Time: 8 ms (41.19%) | Memory: 21.3 MB (19.11%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        max1 = float("-inf")
+        count = 0
+
+        for i in nums:
+            if (count == 0):
+                max1 = i
+            if (i == max1):
+                count += 1
+            else:
+                count -= 1
+        
+        count = 0
+
+        for i in nums:
+            if (i == max1):
+                count += 1
+        
+        if (count > len(nums) // 2):
+            return max1
