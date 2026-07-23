7ad7617fccf90844d7e240c6820f85812abed4a8
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-07-22 20:57:31 -0600

    Time: 56 ms (32.23%) | Memory: 30.7 MB (82.56%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -6,7 +6,7 @@ class Solution:
 
         if (nums[0] > 0 and nums[0] != 1):
             return 1
-        if (nums[n - 1] < 0):
+        if (nums[n - 1] <= 0):
             return 1
 
         while (i < n - 1):
