36c0dc22598a13e704df83ab4891bcfd6c9d11f2
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-08-03 22:02:04 -0600

    Time: 164 ms (28.78%) | Memory: 21.1 MB (54.56%) - LeetSync

--- 2139-detect-squares/detect-squares.py
+++ 2139-detect-squares/detect-squares.py
@@ -3,29 +3,28 @@ from collections import defaultdict, Counter
 class DetectSquares:
 
     def __init__(self):
-        self.d = defaultdict(Counter)
+        self.d = defaultdict(Counter)        
 
     def add(self, point: List[int]) -> None:
         self.d[point[0]][point[1]] += 1
 
     def count(self, point: List[int]) -> int:
         x, y = point
+        count = 0
 
         if (x not in self.d):
             return 0
         
-        result = 0
-
         for i in self.d:
             if (i == x):
                 continue
             
             d1 = abs(i - x)
-            result += (self.d[i][y] * self.d[x][y + d1] * self.d[i][y + d1])
-            result += (self.d[i][y] * self.d[x][y - d1] * self.d[i][y - d1])
-        
-        return result
 
+            count += (self.d[i][y] * self.d[x][y + d1] * self.d[i][y + d1])
+            count += (self.d[i][y] * self.d[x][y - d1] * self.d[i][y - d1])
+        
+        return count
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()
