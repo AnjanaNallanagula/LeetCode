954a93150b83c3b2df278c0252faa638813afb0d
Anjana Nallanagula <68939759+AnjanaNallanagula@users.noreply.github.com>
2026-08-08 17:39:31 -0600

    Time: 148 ms (5.06%) | Memory: 31.9 MB (38.38%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countGoodNodes(self, root, ls, path, max1):
+        if (root == None):
+            return
+        
+        if ((len(path) == 0) or (len(path) and root.val >= max1[0])):
+            ls[0] += 1
+            path.append(root.val)
+            max1[0] = root.val
+        
+        self.countGoodNodes(root.left, ls, path, max1)
+        self.countGoodNodes(root.right, ls, path, max1)
+
+        if (max1[0] == root.val):
+            path.pop()
+            max1[0] = max(path) if (path) else -1
+    
+    def goodNodes(self, root: TreeNode) -> int:
+        ls = [0]
+        path = []
+        max1 = [-1]
+
+        self.countGoodNodes(root, ls, path, max1)
+
+        return ls[0]
